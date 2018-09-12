a811fd45ba154728cc8abfca2ee63671e7fead34
Spencer Casteel <spencercasteel@interapts-macbook-pro-9.local>
2018-09-12 10:08:39 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Spencer Casteel on 9/12/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
